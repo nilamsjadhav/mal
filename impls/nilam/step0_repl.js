b9e01d87346ab7681d0ee758b22a0d961bd72cbc
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2023-05-26 11:08:30 +0530

    implemented step 0

---
+++ impls/nilam/step0_repl.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const READ = (arg) => arg;
+
+const EVAL = (arg) => arg;
+
+const PRINT = (arg) => arg;
+
+const rep = (arg) => (PRINT(EVAL(READ(arg))));
+
+const repl = () =>
+  rl.question('user> ', line => {
+    console.log(rep(line));
+    repl();
+  });
+
+repl();
+
